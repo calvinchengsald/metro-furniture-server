f320e9e45570ae3fc850e803c1278d6cdeb63af9
cookies4cream <calvinanvin@gmail.com>
2020-02-08 16:03:32 -0500

    cors header

--- src/main/java/com/metro/controller/GeneralController.java
+++ src/main/java/com/metro/controller/GeneralController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.metro.model.EdgeOption;
 import com.metro.model.ProductInfo;
 import com.metro.repository.ProductInfoRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/product")
 public class GeneralController {
--- src/main/java/com/metro/controller/TypeHiearchyController.java
+++ src/main/java/com/metro/controller/TypeHiearchyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.metro.model.EdgeOption;
 import com.metro.model.TypeHiearchy;
 import com.metro.repository.TypeHiearchyRepository;
 
 @RestController
 @RequestMapping("/typehiearchy")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TypeHiearchyController {
 
 	@Autowired
